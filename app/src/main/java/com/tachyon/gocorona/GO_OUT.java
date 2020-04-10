e4bb1c5051ac5672a580a48f56a2c18f49817471
Pranjal <pranjalpchaudhari99@gmail.com>
2020-04-10 23:36:41 +0530

    new release version

--- app/src/main/java/com/tachyon/gocorona/GO_OUT.java
+++ app/src/main/java/com/tachyon/gocorona/GO_OUT.java
@@ -54,6 +54,12 @@ public class GO_OUT extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
         setContentView(R.layout.activity_go__out);
          time=findViewById(R.id.time);
          date=findViewById(R.id.date);
